88f049d0eb93553624480d3ba72bd46b86d53ae9
MMaiero <matteo.maiero@eurotech.com>
2015-10-15 10:47:57 +0200

    Applied changes to can protocol, cs and mgw implementation.
    Signed-off-by: MMaiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.protocol.can/src/main/java/org/eclipse/kura/protocol/can/CanConnectionServiceImpl.java
+++ kura/org.eclipse.kura.protocol.can/src/main/java/org/eclipse/kura/protocol/can/CanConnectionServiceImpl.java
@@ -18,38 +18,43 @@ import de.entropia.can.CanSocket.Mode;
 public class CanConnectionServiceImpl implements CanConnectionService {
 
 	private static final Logger s_logger = LoggerFactory.getLogger(CanConnectionServiceImpl.class);
-	
+
 	protected void activate(ComponentContext componentContext) {
 		s_logger.info("activating CanConnectionService");
 	}
-	
+
 	protected void deactivate(ComponentContext componentContext) {
 	}
-	
+
 	@Override
 	public void sendCanMessage(String ifName, int canId, byte[] message)
 			throws KuraException, IOException {
 		if(message.length>8)
 			throw new KuraException(KuraErrorCode.INTERNAL_ERROR, "CAN send : Incorrect frame length");
-			
-    	try {
-			CanSocket socket = new CanSocket(Mode.RAW);
+
+		CanSocket socket= null;
+		try {
+			socket = new CanSocket(Mode.RAW);
 			socket.setLoopbackMode(false);
 			CanInterface canif = new CanInterface(socket, ifName);
 			socket.bind(canif);
 			socket.send(new CanFrame(canif, new CanId(canId), message));
 			//s_logger.debug("message sent on " + ifName);
-			socket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw e;
+		} finally {
+			if(socket != null){
+				socket.close();
+			}
 		}
 	}
 
 	@Override
 	public CanMessage receiveCanMessage(int can_id, int can_mask) throws KuraException, IOException {
+		CanSocket socket= null;
 		try {
-			CanSocket socket = new CanSocket(Mode.RAW);
+			socket = new CanSocket(Mode.RAW);
 			socket.setLoopbackMode(false);
 			socket.bind(CanSocket.CAN_ALL_INTERFACES);
 			if(can_id>=0){
@@ -61,13 +66,12 @@ public class CanConnectionServiceImpl implements CanConnectionService {
 			CanMessage cm = new CanMessage();
 			cm.setCanId(ci.getCanId_SFF());
 			cm.setData(cf.getData());
-			socket.close();
 			return cm;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} finally {
+			if(socket != null){
+				socket.close();
+			}
 		}
-		return null;
 	}
 
 }
